b10b375d986aa1ca07bdb7318f32bb45a085d655
SRE <Nerocross507@gmail.com>
2024-05-15 10:26:39 +0300

    manufactured_at make migrations, migrate

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -13,15 +14,18 @@ class Category(models.Model):
         verbose_name_plural = 'категории'
 
 
+NULLABLE = {'null': True, 'blank': True}
+
+
 class Product(models.Model):
     name = models.CharField(max_length=250, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание', null=True, blank=True)
-    preview = models.ImageField(upload_to='media/', null=True, blank=True, verbose_name='Превью')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(upload_to='media/', **NULLABLE, verbose_name='Превью')
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата последнего изменения')
-    manufactured_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата производства продукта')
+    manufactured_at = models.DateTimeField(**NULLABLE, verbose_name='Дата производства продукта')
 
     def __str__(self):
         return f'{self.name}'
